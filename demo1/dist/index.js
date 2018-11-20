2744b3d5f5821e9a259bf6968f65fb648985be23
wilson9898 <1249613@qq.com>
2018-11-20 11:03:18 +0800

    webpack demo
    my webpack demo

---
+++ demo1/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var foo = function foo() {
+    var a = 20;
+    var b = 30;
+    return a + b;
+};
---
+++ demo1/src/index.js
@@ -0,0 +1,5 @@
+const foo = () => {
+    const a = 20;
+    const b = 30;
+    return a + b;
+ }
